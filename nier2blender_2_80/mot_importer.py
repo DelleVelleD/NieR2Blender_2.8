ad3cb467c6cd86919d297c63d892ac36e87b7a32
dellevelled <delle.caj@gmail.com>
2020-08-25 10:30:50 -0400

    Fix pose bone location

--- nier2blender_2_80/mot_importer.py
+++ nier2blender_2_80/mot_importer.py
@@ -6,12 +6,6 @@ importlib.reload(nier2blender_2_80.mot_importer)
 import bpy, bmesh, math, mathutils
 import nier2blender_2_80.mot as MOT
 
-def show_message(message = "", title = "Message Box", icon = 'INFO'):
-	def draw(self, context):
-		self.layout.label(text = message)
-		self.layout.alignment = 'CENTER'
-	bpy.context.window_manager.popup_menu(draw, title = title, icon = icon)
-
 def format_motion_data(frame_count, records):
 	formatted_records = {} #{boneNumber: POSX[], POSY[], POSZ[], ROTX[], ROTY[], ROTZ[], SCALEX[], SCALEY[], SCALEZ[]}
 	
@@ -38,7 +32,7 @@ def format_motion_data(frame_count, records):
 				if value is None:
 					print(record.offset, record.bone_id, i, record.valueType, record.recordType)
 		else:
-			print('[MOT-Error] Unknown value type:%d' % record.valueType)
+			print('[MOT-Error] Unknown value type: %d' % record.valueType)
 		
 	#fill in missing records
 	default_value = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0]
@@ -54,43 +48,50 @@ def format_motion_data(frame_count, records):
 		scale_frames = []
 		for i in range(frame_count):
 			pos_frames.append([i, mathutils.Vector([bone_records[0][i], bone_records[1][i], bone_records[2][i]])])
-			rot = mathutils.Euler((bone_records[3][i], bone_records[4][i], bone_records[5][i]), 'XYZ')
-			quat = rot.to_quaternion()
-			rot_frames.append([i, quat])
+			rot_frames.append([i, mathutils.Euler((bone_records[3][i], bone_records[4][i], bone_records[5][i]), 'XYZ')])
 			scale_frames.append([i, mathutils.Vector([bone_records[6][i], bone_records[7][i], bone_records[8][i]])])
 	
 		motion_data[bone_id] = [pos_frames, rot_frames, scale_frames]
 		
 	return motion_data
 
-def construct_action(mot, motion_data, armature): #(mot.py MOT object, motion data from above, blender armature object)
+def construct_action(mot, motion_data, active, bind_pose): #(mot.py MOT object, motion data from above, blender active object)
 	print('[+] importing motion %s' % mot.motionName)
 	action = bpy.data.actions.new(name=mot.motionName)
 	action.use_fake_user = True
-	if armature.animation_data is None:
-		armature.animation_data_create()
-	armature.animation_data.action = action
+	if active.animation_data is None:
+		active.animation_data_create()
+	active.animation_data.action = action
 	
-	bpy.context.view_layer.objects.active = armature 
-	bpy.ops.object.mode_set(mode='POSE') #Set armature to pose mode
-	bone_mapping = armature["bone_mapping"] #Get bones from armature
+	bpy.context.view_layer.objects.active = active
+	bpy.ops.object.mode_set(mode='POSE') #Set active to pose mode
+	bone_mapping = active["bone_mapping"] #Get bones from active
 	pose_bones = bpy.context.view_layer.objects.active.pose.bones 
 	
-	print('[MOT-Info] armature.name: %s' % armature.name)
+	print('[MOT-Info] active.name: %s' % active.name)
 	print('[MOT-Info] bpy.context.view_layer.objects.active.name: %s' % bpy.context.view_layer.objects.active.name)
 	
 	used_bones = []
+	i = 0
 	for bone_number, values in motion_data.items(): #loop through bones
+		i+=1
 		pos_values, rot_values, scale_values = values
 		bone_name = bone_mapping.get(str(bone_number))
-		
+		if bone_number == 65535 and bone_name is None:
+			bone_name = bone_mapping.get(str(4095))
+
 		if bone_name is None:
-			print('[MOT-Error] bone_number = %d not found in bone_mapping.' % bone_number)
+			print('[MOT-Error] bone_number = %d not found in bone_mapping' % bone_number)
 			continue
 			
 		pose_bone = pose_bones.get(bone_name)
 		if pose_bone is None:
-			print('[MOT-Error] %s not found in armature.pose.bones.' % bone_name)
+			print('[MOT-Error] %s not found in active.pose.bones' % bone_name)
+			continue
+
+		edit_bone_matrix = bind_pose[bone_name]
+		if edit_bone_matrix is None:
+			print('[MOT-Error] %s not found in bind_pose' % bone_name)
 			continue
 		
 		if bone_name not in used_bones:
@@ -100,27 +101,31 @@ def construct_action(mot, motion_data, armature): #(mot.py MOT object, motion da
 		if pos_values is not None:
 			for pos_value in pos_values:
 				frame = pos_value[0] + 1 #set initial frame to 1
-				pose_bone.location = pos_value[1]
+				if pos_value[1] == mathutils.Vector():
+					pose_bone.location = mathutils.Vector([0,0,0])
+				else:
+					pose_bone.location = -(pose_bone.bone.head - pos_value[1])
 				pose_bone.keyframe_insert("location", index=-1, frame=frame)
 				#print('[MOT-Info] Inserting location keyframe for %s at %s on frame %d' % (bone_name, str(pose_bone.location), frame))
+				#if frame == 1:
+					#print('bone.head: %s, pos_value[1]" %s' % (str(pose_bone.bone.head), str(pos_value[1])))
 		else:
-			pose_bone.location = mathutils.Vector([0,0,0]) #if no position values, set to 0,0,0, not moving the bone
+			pose_bone.location = mathutils.Vector([0,0,0])
 			pose_bone.keyframe_insert("location", index=-1, frame=1)
 			#print('[MOT-Info] Inserting location keyframe for %s at %s on frame %d' % (bone_name, str(pose_bone.location), frame))
-		print('[MOT-Info] Finished inserting %d location keyframes' % len(pos_values))
 		
 		#rotation keyframes
 		if rot_values is not None:
 			for rot_value in rot_values:
 				frame = rot_value[0] + 1
-				pose_bone.rotation_quaternion = rot_value[1]
+				quat = rot_value[1].to_quaternion()
+				pose_bone.rotation_quaternion = quat
 				pose_bone.keyframe_insert("rotation_quaternion", index=-1, frame=frame)
 				#print('[MOT-Info] Inserting rotation keyframe for %s at %s on frame %d' % (bone_name, str(pose_bone.rotation_quaternion), frame))
 		else:
 			pose_bone.rotation_quaternion = mathutils.Quaternion([1, 0, 0, 0])
-			pose_bone.keyframe_insert("rotation_quaternion", index=-1, frame=frame)
+			pose_bone.keyframe_insert("rotation_quaternion", index=-1, frame=1)
 			#print('[MOT-Info] Inserting rotation keyframe for %s at %s on frame %d' % (bone_name, str(pose_bone.rotation_quaternion), frame))
-		print('[MOT-Info] Finished inserting %d rotation keyframes' % len(rot_values))
 
 		#scale keyframe
 		if scale_values is not None:
@@ -133,29 +138,37 @@ def construct_action(mot, motion_data, armature): #(mot.py MOT object, motion da
 			pose_bone.scale = mathutils.Vector([1, 1, 1])
 			pose_bone.keyframe_insert("scale", index=-1, frame=1)
 			#print('[MOT-Info] Inserting scale keyframe for %s at %s on frame %d' % (bone_name, str(pose_bone.scale), frame))
-		print('[MOT-Info] Finished inserting %d scale keyframes' % len(scale_values))
-			
-	print('[MOT-Info] Motion Bones Used: ')
+		print('[MOT-Info] Finished inserting keyframes for bone %d / %d' % (i, len(motion_data.items())))
+
 	uBones = ''
 	for boneNum in used_bones:
 		uBones += boneNum + ', '
-	print(uBones)
+	print('[MOT-Info] Motion Bones Used: ' + uBones)
 	
 	for fcurve in action.fcurves:
 		for keyframe_point in fcurve.keyframe_points:
 			keyframe_point.interpolation = 'LINEAR'
 			
 	bpy.ops.object.mode_set(mode='OBJECT')
-	
-def main(mot_fp, armature):	
+
+def get_bind_pose(active):
+	bpy.ops.object.mode_set(mode='EDIT')
+	matrices = {}
+	for bone in bpy.data.armatures[active.name + 'Amt'].edit_bones:
+		matrices[bone.name] = bone.matrix
+	bpy.ops.object.mode_set()
+	return matrices
+
+def main(mot_fp, active):
 	bpy.ops.object.mode_set()
 	
 	mot = MOT.MOT(mot_fp)
 	motion = format_motion_data(mot.frameCount, mot.records)
-	construct_action(mot, motion, armature)
+	bind_pose = get_bind_pose(active)
+	construct_action(mot, motion, active, bind_pose)
 	
 	print('Motion importing finished.')
 	return {'FINISHED'}
 
 if __name__ == '__main__':
-		main('', None)
+	print('[MOTError] Cant use through command line.')
